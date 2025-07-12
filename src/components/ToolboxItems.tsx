42fa20b4f44034c2a1117c684afa9a026fce4ed5
Rizki Nabil <rizkinabil@student.telkomuniversity.ac.id>
2025-07-12 22:49:28 +0700

    feat: about section screen

---
+++ src/components/ToolboxItems.tsx
@@ -0,0 +1,36 @@
+import { TechIcon } from '@/components/TechIcon';
+import { twMerge } from 'tailwind-merge';
+
+export const ToolboxItems = ({
+  items,
+  className,
+  itemsWrapperClassname,
+}: {
+  className?: string;
+  itemsWrapperClassname?: string;
+  items: {
+    title: string;
+    iconType: React.ElementType;
+  }[];
+}) => {
+  return (
+    <div
+      className={twMerge(
+        'flex [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)]',
+        className
+      )}
+    >
+      <div className={twMerge('flex flex-none py-0.5 gap-6 pr-6', itemsWrapperClassname)}>
+        {items.map((item) => (
+          <div
+            key={item.title}
+            className="inline-flex items-center gap-4 py-2 px-3 outline outline-2 outline-white/10 rounded-lg"
+          >
+            <TechIcon component={item.iconType} />
+            <span className="font-semibold">{item.title}</span>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
